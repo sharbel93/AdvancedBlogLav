61a479bef7f1bd25c427cd067f344a50ec12c1ee
sharbel93 <sharbel.chris@gmail.com>
2018-08-29 14:31:49 +0300

    configured js,css,database & mail

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,15 +8,17 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Buefy from 'buefy';
 
+Vue.usse(Buefy);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
